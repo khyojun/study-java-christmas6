6cc8c2a66f2ca6a76d27949102818c8e971ccf6d
김효준 <khyojun1104@gmail.com>
2023-11-27 18:12:51 +0900

    refactor(christmas): 더 쉬운 로직으로 검증 작업 변경!

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -5,44 +5,30 @@ import java.util.regex.Pattern;
 
 public class Date {
 
-    private static final String NUMBER_PATTERN = "[1-9]+";
+    private static final int START_DATE=1;
+    private static final int FINAL_DATE=31;
+
     private static final String DATE_EXCEPTION_MESSAGE= "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     private int date;
 
     public Date(String beforeConvertDate) {
         try {
-            validateNumber(beforeConvertDate);
-            int convertedDate = convert(beforeConvertDate);
+            int convertedDate = Integer.parseInt(beforeConvertDate);
             validateRange(convertedDate);
             this.date=date;
-        }catch (DateException dateError){
+        }catch (DateException | NumberFormatException dateError){
             throw new IllegalArgumentException(dateError.getMessage());
         }
     }
 
     private void validateRange(int convertedDate) {
-        if(convertedDate < 1 || convertedDate > 31)
+        if(convertedDate < START_DATE || convertedDate > FINAL_DATE)
             throw new DateException(DATE_EXCEPTION_MESSAGE);
     }
 
-    private int convert(String beforeConvertDate) {
-        return Integer.parseInt(beforeConvertDate);
-    }
 
-    private void validateNumber(String beforeConvertDate) {
-        if(isNotNumber(beforeConvertDate)){
-            throw new DateException(DATE_EXCEPTION_MESSAGE);
-        }
-    }
 
-    private boolean isNotNumber(String beforeConvertDate) {
-        return !Pattern.matches(NUMBER_PATTERN, beforeConvertDate);
-    }
-
-    public int getDate() {
-        return date;
-    }
 
 
 }
