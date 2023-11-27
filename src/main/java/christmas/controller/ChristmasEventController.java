16a6d9f968a6de2a4ad45ad61d1b9f8647f60371
김효준 <khyojun1104@gmail.com>
2023-11-27 16:54:57 +0900

    chore(christmas): 기본적인 controller 틀 제작!

---
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -0,0 +1,22 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasEventController {
+
+    private InputView inputView;
+    private OutputView outputView;
+
+    public ChristmasEventController(InputView inputView, OutputView outputView) {
+        this.inputView=inputView;
+        this.outputView=outputView;
+    }
+
+    public void eventStart() {
+
+
+
+
+    }
+}
